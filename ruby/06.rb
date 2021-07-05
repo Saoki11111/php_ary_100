e5676a320a601ff93b9e17c5a3f661c0018fe876
Saoki11111 <t.aoki11111@gmail.com>
2021-07-05 20:07:06 +0900

    ruby で fill

---
+++ ruby/06.rb
@@ -0,0 +1,20 @@
+# 中身が全て $in1 で、要素数が $in2 個の配列を作ってください。
+
+in1 = 'x'
+in2 = 5
+
+# //////////////////////////////////
+# //       SOME CODE HERE         //
+# //////////////////////////////////
+
+ary = []
+p ary.fill(in1, 0.. in2)
+
+# /*
+# array (size=5)
+#   0 => string 'x' (length=1)
+#   1 => string 'x' (length=1)
+#   2 => string 'x' (length=1)
+#   3 => string 'x' (length=1)
+#   4 => string 'x' (length=1)
+# */
